17e7e6de23dc493b8292182dd1a6e485036af741
Andres Ponciano <andres.ponciano123@gmail.com>
2020-07-09 19:55:04 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <div className="home-body">
+                <h1>poncitions</h1>
+                {/* <img src={require('../images/finaltree.jpg')} /> */}
+            </div>
+        </>
+    );
+};
+
+export default App;
