f18cffd5a883a67c7a49517a1233e23e641080f8
Andres Ponciano <andres.ponciano123@gmail.com>
2020-07-09 20:17:52 -0400

    added icons and bg in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const App = () => {
     return (
         <>
             <div className="home-body">
-                <h1>poncitions</h1>
+                <div className="brands">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <FontAwesomeIcon icon={faGithub} />
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </div>
+                <div className="brand">poncitions moon</div>
+                <div className="menu">
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
                 {/* <img src={require('../images/finaltree.jpg')} /> */}
             </div>
         </>
