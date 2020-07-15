ee303e2b1608d66e2b13f4c114a3ec26d1b0de60
Andres Ponciano <andres.ponciano123@gmail.com>
2020-07-14 23:43:44 -0400

    changing things to css emotion

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import { mystyle } from '../styles/frontStyles';
 const App = () => {
+    const [sidebar, setSidebar] = useState(false);
+
+    // const mystyle = css`
+    //     color: hotpink;
+    //     backgroundimage: url('../images/finaltree.jpg');
+    // `;
+    console.log(sidebar);
     return (
-        <>
-            <div className="home-body">
-                <div className="brands">
-                    <FontAwesomeIcon className="icon fa-lg" icon={faEnvelope} />
-                    <FontAwesomeIcon className="icon fa-lg" icon={faGithub} />
-                    <FontAwesomeIcon className="icon fa-lg" icon={faLinkedin} />
-                </div>
-                <div className="brand">
-                    <h1 className="brand-name">poncitions</h1>
-                    <img
-                        className="brand-logo"
-                        src={require('../images/moonlogo.jpg')}
-                    />
-                </div>
-                <div className="menu">
-                    <FontAwesomeIcon
-                        className="menu-icon fa-lg"
-                        icon={faBars}
-                    />
-                </div>
+        <div className="home-body" css={mystyle}>
+            <div className="brands">
+                <FontAwesomeIcon className="icon fa-lg" icon={faEnvelope} />
+                <FontAwesomeIcon className="icon fa-lg" icon={faGithub} />
+                <FontAwesomeIcon className="icon fa-lg" icon={faLinkedin} />
             </div>
-        </>
+            <div className="brand">
+                <h1 className="brand-name">poncitions</h1>
+                <img
+                    className="brand-logo"
+                    src={require('../images/moonlogo.jpg')}
+                />
+            </div>
+            <div className="menu">
+                <FontAwesomeIcon
+                    className="menu-icon fa-lg"
+                    icon={faBars}
+                    onClick={() => setSidebar(true)}
+                />
+            </div>
+        </div>
     );
 };
 
---
+++ src/styles/frontStyles.js
@@ -0,0 +1,9 @@
+/**@jsx jsx */
+import { jsx, css } from '@emotion/core';
+import styled from '@emotion/styled';
+
+export const mystyle = css`
+    display: grid;
+    grid-template-columns: 1fr 14fr 5fr;
+    grid-template-rows: 100vh;
+`;
