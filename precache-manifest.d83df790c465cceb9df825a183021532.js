02d23859f2da511a81bb1d45c1d8bfb2d3e845cb
Andres Ponciano <andres.ponciano123@gmail.com>
2020-09-06 14:44:39 -0400

    Updates

---
+++ precache-manifest.d83df790c465cceb9df825a183021532.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342c7ef6f12fd344091f21a28c3cd0a9",
+    "url": "/poncitions/index.html"
+  },
+  {
+    "revision": "9983b2232e2c42297311",
+    "url": "/poncitions/static/css/main.2b8fb396.chunk.css"
+  },
+  {
+    "revision": "e6ca698338530af07870",
+    "url": "/poncitions/static/js/2.68abf030.chunk.js"
+  },
+  {
+    "revision": "a40fee5c54c2916b9d7657520c61b947",
+    "url": "/poncitions/static/js/2.68abf030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9983b2232e2c42297311",
+    "url": "/poncitions/static/js/main.211a3219.chunk.js"
+  },
+  {
+    "revision": "956b8ec2085a47980f00",
+    "url": "/poncitions/static/js/runtime-main.e6e5e5b8.js"
+  },
+  {
+    "revision": "8eb197c0b7746fff5c112a6fe15bc40a",
+    "url": "/poncitions/static/media/finaltree.8eb197c0.jpg"
+  },
+  {
+    "revision": "b56f17bd77707fe57f261596b5cd25e4",
+    "url": "/poncitions/static/media/finaltreedark2.b56f17bd.png"
+  },
+  {
+    "revision": "8b260ca7fb49f751c702a7177c8e60f2",
+    "url": "/poncitions/static/media/jobgroups_ss.8b260ca7.png"
+  },
+  {
+    "revision": "211ef7c81f0b639d3f3a0a7697a37ab7",
+    "url": "/poncitions/static/media/miniyoutube_ss.211ef7c8.png"
+  },
+  {
+    "revision": "360bb96a029de1c34b27906bb7d157bd",
+    "url": "/poncitions/static/media/myface.360bb96a.png"
+  },
+  {
+    "revision": "ed1154066a1eebf34cc53166d357e28c",
+    "url": "/poncitions/static/media/nothing_yet.ed115406.png"
+  },
+  {
+    "revision": "e57cf025546dd13a2445e7225662f5e9",
+    "url": "/poncitions/static/media/poncitions_ss.e57cf025.png"
+  }
+]);
